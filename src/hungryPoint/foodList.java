d34bb38f282b11a7cb06d147ae0671eeb298c9b5
anu-bansal <anubansal0707@gmail.com>
2018-02-27 06:42:30 +0530

    first commit

---
+++ src/hungryPoint/foodList.java
@@ -0,0 +1,27 @@
+package hungryPoint;
+import java.io.IOException;
+
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/foodList")
+public class foodList extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    public foodList() {
+        super();
+       
+    }
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		String restaurantRequested=request.getParameter("restaurant");
+		out.print(restaurantRequested);
+
+	}
+}
+
